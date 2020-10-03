a1dec65fdee3f8e4d5f09d3917e008ac8ef4dca9
ograsdijk <o.grasdijk@gmail.com>
2020-10-02 20:13:44 -0400

    various

--- rotational cooling/utils/hdf_utils.py
+++ rotational cooling/utils/hdf_utils.py
@@ -30,3 +30,20 @@ def load_measurement_data_devices(fname, dset_name, devices = ('Laser Lock',
                 pxie_time[int(dset.split('_')[-1])] = f[dset_name]['readout']['PXIe-5171'][dset].attrs[f'{ts_ch} : timestamp']
 
     return pxie, pxie_time, data_devices
+
+def load_measurement_data_devices_attrs(fname, dset_name, devices = ('Laser Lock',
+                          'Wavelength', 'ZaberTMM'), ts_ch = 'ch0'):
+    # loading data from HDF file
+    with h5py.File(fname, 'r') as f:
+        data_devices = {}
+        for device in devices:
+            data_devices[device] = f[dset_name]['readout'][device][()]
+        pxie_time = {}
+        pxie = {}
+        pxie_attrs = {}
+        for dset in f[dset_name]['readout']['PXIe-5171']:
+            if 'events' not in dset:
+                pxie[int(dset.split('_')[-1])] = f[dset_name]['readout']['PXIe-5171'][dset][()]
+                pxie_time[int(dset.split('_')[-1])] = f[dset_name]['readout']['PXIe-5171'][dset].attrs[f'{ts_ch} : timestamp']
+                pxie_attrs[int(dset.split('_')[-1])] = {k:v for k,v in f[dset_name]['readout']['PXIe-5171'][dset].attrs.items()}
+    return pxie, pxie_time, pxie_attrs, data_devices
